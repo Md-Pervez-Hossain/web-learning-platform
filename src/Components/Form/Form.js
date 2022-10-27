6e60fc83e076516f397a872b6344ec472f9c4efb
Md-Pervez-Hossain <p.hossain9254@gmail.comm>
2022-10-27 13:57:49 +0600

    checkout page update

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -110,14 +110,14 @@ const Form = () => {
         </div>
         <button
           type="submit"
-          className="text-white bg-primary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium  text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white bg-primary focus:ring-4 mb-3 focus:outline-none focus:ring-blue-300 font-medium  text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Place Order
         </button>
         <Link to="/courses">
           <button
             type="submit"
-            className="text-white bg-primary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:ml-3"
+            className="text-white bg-primary  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:ml-3"
           >
             Cancle
           </button>
