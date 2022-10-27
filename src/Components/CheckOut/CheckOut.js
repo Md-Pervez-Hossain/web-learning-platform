65525a978b2b7c2421b367ffb5861a632ee00f43
Md-Pervez-Hossain <p.hossain9254@gmail.comm>
2022-10-27 12:40:48 +0600

    update check out page

--- src/Components/CheckOut/CheckOut.js
+++ src/Components/CheckOut/CheckOut.js
@@ -18,8 +18,17 @@ const CheckOut = () => {
             For Premium Access You need to pay :
             <span className="text-primary "> $ {price}</span>
           </p>
+          <div className="mt-5">
+            <h2 className="text-2xl font-bold my-4 text-center">We Support</h2>
+            <img
+              className="mt-2"
+              src="https://t4.ftcdn.net/jpg/04/73/84/61/360_F_473846184_0k637f6855ZJqaulKqAmgJTEVGVibR1P.jpg"
+              alt=""
+            />
+          </div>
         </div>
-        <div className="md:pt-32 pt-16">
+        <div className=" pt-10">
+          <h2 className="text-2xl font-bold my-4 ">Fill this out for order</h2>
           <Form></Form>
         </div>
       </div>
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -5,33 +5,33 @@ const Form = () => {
   return (
     <div>
       <form>
-        <div class="grid gap-6 mb-6 md:grid-cols-2">
+        <div className="grid gap-6 mb-6 md:grid-cols-2">
           <div>
             <label
-              for="first_name"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              htmlFor="first_name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
               First name
             </label>
             <input
               type="text"
               id="first_name"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="John"
               required=""
             />
           </div>
           <div>
             <label
-              for="last_name"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              htmlFor="last_name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
               Last name
             </label>
             <input
               type="text"
               id="last_name"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Doe"
               required=""
             />
@@ -39,82 +39,89 @@ const Form = () => {
 
           <div>
             <label
-              for="company"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              htmlFor="company"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
               Transaction Id
             </label>
             <input
               type="text"
               id="company"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Transaction Id"
               required=""
             />
           </div>
           <div>
             <label
-              for="phone"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              htmlFor="phone"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
               Phone number
             </label>
             <input
               type="tel"
               id="phone"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="123-45-678"
               pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
               required=""
             />
           </div>
         </div>
-        <div class="mb-6">
+        <div className="mb-6">
           <label
-            for="email"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
           >
             Email address
           </label>
           <input
             type="email"
             id="email"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="john.doe@company.com"
             required=""
           />
         </div>
 
-        <div class="flex items-start mb-6">
-          <div class="flex items-center h-5">
+        <div className="flex items-start mb-6">
+          <div className="flex items-center h-5">
             <input
               id="remember"
               type="checkbox"
               value=""
-              class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+              className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
               required=""
             />
           </div>
           <label
-            for="remember"
-            class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+            htmlFor="remember"
+            className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-400"
           >
-            I agree with the{" "}
+            I agree with the
             <Link
               href="#"
-              class="text-blue-600 hover:underline dark:text-blue-500"
+              className=" ml-2 text-primary hover:underline dark:text-blue-500"
             >
-              terms and conditions
+              terms and conditions .
             </Link>
-            .
           </label>
         </div>
         <button
           type="submit"
-          class="text-white bg-primary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white bg-primary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium  text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Submit
+          Place Order
         </button>
+        <Link to="/courses">
+          <button
+            type="submit"
+            className="text-white bg-primary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:ml-3"
+          >
+            Cancle
+          </button>
+        </Link>
       </form>
     </div>
   );
