c2dcae7b39698533810fb07a02fb9189051ddc91
Md-Pervez-Hossain <p.hossain9254@gmail.comm>
2022-10-27 16:30:30 +0600

    organize route

--- src/App.js
+++ src/App.js
@@ -1,84 +1,9 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
-import Blog from "./Components/Blog/Blog";
-import CheckOut from "./Components/CheckOut/CheckOut";
-import Courses from "./Components/Courses/Courses";
-import DetailsCourse from "./Components/Courses/DetailsCourse";
-import ErrorPage from "./Components/ErrorPage/ErrorPage";
-import Faq from "./Components/Faq/Faq";
 
-import Home from "./Components/Home/Home";
-import Main from "./Components/Layout/Main";
-import Login from "./Components/Login/Login";
-import PrivateRoute from "./Components/PrivateRoute/PrivateRoute";
-import Register from "./Components/Register/Register";
+import { router } from "./Components/Routes/Routes";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      errorElement: <ErrorPage></ErrorPage>,
-      children: [
-        {
-          path: "/",
-          element: <Home></Home>,
-        },
-        {
-          path: "/home",
-          element: <Home></Home>,
-        },
-        {
-          path: "/courses",
-          loader: () =>
-            fetch(
-              " https://b610-learning-platform-server-side-md-pervez-hossain.vercel.app/courses"
-            ),
-          element: <Courses></Courses>,
-        },
-        {
-          path: "/courses/:id",
-          loader: ({ params }) =>
-            fetch(
-              ` https://b610-learning-platform-server-side-md-pervez-hossain.vercel.app/courses/${params.id}`
-            ),
-          element: (
-            <PrivateRoute>
-              <DetailsCourse></DetailsCourse>
-            </PrivateRoute>
-          ),
-        },
-        {
-          path: "/blog",
-          element: <Blog></Blog>,
-        },
-        {
-          path: "/login",
-          element: <Login></Login>,
-        },
-        {
-          path: "/faq",
-          element: <Faq></Faq>,
-        },
-        {
-          path: "/register",
-          element: <Register></Register>,
-        },
-        {
-          path: "/checkout/:id",
-          loader: ({ params }) =>
-            fetch(
-              ` https://b610-learning-platform-server-side-md-pervez-hossain.vercel.app/checkout/${params.id}`
-            ),
-          element: (
-            <PrivateRoute>
-              <CheckOut></CheckOut>
-            </PrivateRoute>
-          ),
-        },
-      ],
-    },
-  ]);
   return (
     <div>
       <RouterProvider router={router}></RouterProvider>
---
+++ src/Components/Routes/Routes.js
@@ -0,0 +1,74 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Blog from "../Blog/Blog";
+import CheckOut from "../CheckOut/CheckOut";
+import Courses from "../Courses/Courses";
+import DetailsCourse from "../Courses/DetailsCourse";
+import ErrorPage from "../ErrorPage/ErrorPage";
+import Faq from "../Faq/Faq";
+import Home from "../Home/Home";
+import Main from "../Layout/Main";
+import Login from "../Login/Login";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import Register from "../Register/Register";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/home",
+        element: <Home></Home>,
+      },
+      {
+        path: "/courses",
+        loader: () =>
+          fetch(
+            " https://b610-learning-platform-server-side-md-pervez-hossain.vercel.app/courses"
+          ),
+        element: <Courses></Courses>,
+      },
+      {
+        path: "/courses/:id",
+        loader: ({ params }) =>
+          fetch(
+            ` https://b610-learning-platform-server-side-md-pervez-hossain.vercel.app/courses/${params.id}`
+          ),
+        element: <DetailsCourse></DetailsCourse>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/faq",
+        element: <Faq></Faq>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/checkout/:id",
+        loader: ({ params }) =>
+          fetch(
+            ` https://b610-learning-platform-server-side-md-pervez-hossain.vercel.app/checkout/${params.id}`
+          ),
+        element: (
+          <PrivateRoute>
+            <CheckOut></CheckOut>
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+]);
